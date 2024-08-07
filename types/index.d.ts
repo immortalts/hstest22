e77460e0914ea3e5e83d6d2444c66e9e97dccd18
你好小羊 <2011543968@qq.com>
2024-08-07 16:17:47 +0800

    6

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,20 @@
 import type { App } from 'vue'
 
+export const version: string
 export const install: (app: App) => void
 
 export * from './basicComponent'
 export * from './button'
+export * from './styleVars'
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    VarBasicComponent: typeof import('@varlet/ui')['_BasicComponentComponent']
+    VarButton: typeof import('@varlet/ui')['_ButtonComponent']
+    VarStyleVars: typeof import('@varlet/ui')['_StyleVarsComponent']
+  }
+
+  export interface ComponentCustomProperties {
+    
+  }
+}
